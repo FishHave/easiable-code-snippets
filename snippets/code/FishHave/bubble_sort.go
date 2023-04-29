c34bc685225bb6d16798d7ad25534198c6de10f8
FishHave <104718849+FishHave@users.noreply.github.com>
2023-04-29 23:45:29 +0800

    Add bubble sort for Go into main file

--- snippets/code/FishHave/bubble_sort.go
+++ snippets/code/FishHave/bubble_sort.go
@@ -1,10 +1,10 @@
-func sort(arr int[]) int[] {
+func sort(arr <Type>[]) <Type>[] {
 	len := len(arr);
 
     for i := 0; i < len - 1; i++ {
         for j := 0; j < len - 1 - i; j++ {
             if arr[j] > arr[j + 1] {
-                var temp := arr[j+1];
+                temp := arr[j+1];
                 arr[j+1] = arr[j];
                 arr[j] = temp;
         }
